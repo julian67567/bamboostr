a7cd44fa01df9d8df33738ddbf7bac7cb357edce
Jocker <manlioelnum1@hotmail.com>
2016-02-08 01:40:22 -0600

    Bug Redes

--- public/login-twitter.php
+++ public/login-twitter.php
@@ -56,7 +56,7 @@ if(!$_SESSION['sessionid'] || !$_SESSION['user_bamboostr']){
 		header('Location: http://'.getDirUrl(1).'/system.php');
 		} else { 
           //si no existe usuario
-          $userRand = ''.rand(1000000000,9999999999).''.$_SESSION['user_bamboostr'].'';
+          $userRand = ''.$_SESSION['user_bamboostr'].''.rand(1000000000,9999999999).'';
           $passRand = rand(1000000000,9999999999);
 		  //si no existe usuario
 		  $query2=$conn->query("INSERT INTO token (identify,red,ssid,foto,screen_name,password,screen_name_bamboostr,oauth_token,oauth_token_secret,social_networks,idioma,first_ssid,last_ssid) VALUES ('".$user_info->id_str."','twitter','".$_SESSION['sessionid']."','".$user_info->profile_image_url."','".$userRand."','".encriptar($passRand)."','".$user_info->screen_name."','".$credentials['oauth_token']."','".$credentials['oauth_token_secret']."','tw".$user_info->id_str.",','".getUserLanguage()."','".date("d-m-Y")."','".date("d-m-Y")."')") OR DIE(mysqli_error($conn));
@@ -69,8 +69,8 @@ if(!$_SESSION['sessionid'] || !$_SESSION['user_bamboostr']){
           $_SESSION['foto_bamboostr'] = $row["foto_bamboostr"];
           
                   //mandar mail de contraseña
-                $conn->query("INSERT INTO queue_mail (id_token,titulo,mensaje,prioridad) VALUES ('".$_SESSION['id_token']."','Bamboostr: datos de acceso','<br /><br />Muchas Felicidades por registrarte en bamboostr.<br /><br /><center><img src=http://bamboostr.com/images/congrats.png /><br /><br />Te enviamos tus datos de acceso: User: ".$userRand." <br />Pass: ".$passRand."</center><br /><br />','1')") OR DIE(mysqli_error($conn));
-          
+                  $conn->query("INSERT INTO queue_mail (id_token,titulo,mensaje,prioridad) VALUES ('".$_SESSION['id_token']."','Bamboostr: datos de acceso','<br /><br />Muchas Felicidades por registrarte en bamboostr.<br /><br /><center><img src=http://bamboostr.com/images/congrats.png /><br /><br />Te enviamos tus datos de acceso: <br /><br />User: ".$userRand." <br />Pass: ".$passRand."</center><br /><br />','1')") OR DIE(mysqli_error($conn));
+      
                   //insert tutos
                   $conn->query("INSERT INTO tutos (id_token) VALUES ('".$_SESSION['id_token']."')") OR DIE(mysqli_error($conn));
 
